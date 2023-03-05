1cf9ef7adf9c4d4645d5be7d282f7c79ac2e19ee
AZEFUAD <fuadkhs@code.edu.az>
2023-03-05 23:48:00 +0400

    Your file successfully committed.

---
+++ Sultanlı Fuad Collections Homework 2 08.02.2023/Sultanlı Fuad Collections Homework 2 08.02.2023/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Sultanlı_Fuad_Collections_Homework_2_08._02._2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            var student = new Student
+            {
+                Name = "Fuad",
+                Surname = "Sultanlı",
+                Pin = 4824
+            };
+            Checker(students, student);
+
+            var student2 = new Student
+            {
+                Name = "Emin",
+                Surname = "Iskenderov",
+                Pin = 3298
+            };
+            Checker(students, student2);
+
+            var student3 = new Student
+            {
+                Name = "Yalchin",
+                Surname = "Reshidli",
+                Pin = 9845
+            };
+            Checker(students, student3);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.GetDetails());
+            }
+
+            StudentsWithA(students, student3);
+        }
+
+        public static void Checker(List<Student> students, Student student)
+        {
+            var isDublicatedId = students.Any(p => p.Pin == student.Pin);
+
+            if (!isDublicatedId)
+            {
+                students.Add(student);
+            }
+        }
+
+        public static void StudentsWithA(List<Student> classroom, Student student)
+        {
+            var count = classroom.Count(p => p.Name.Contains("a"));
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Sultanlı Fuad Collections Homework 2 08.02.2023/Sultanlı Fuad Collections Homework 2 08.02.2023/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sultanlı_Fuad_Collections_Homework_2_08._02._2023
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Pin { get; set; }
+
+        public string GetDetails()
+        {
+            return ($"Name : {Name} Surname : {Surname} Pin :{Pin}");
+        }
+    }
+}
