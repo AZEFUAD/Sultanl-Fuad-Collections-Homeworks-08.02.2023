0eec8de234d0376263c45c5645f15ff691a643b2
AZEFUAD <121627224+AZEFUAD@users.noreply.github.com>
2023-03-05 23:49:30 +0400

    Add files via upload

---
+++ Sultanlı Fuad Collections Homework 08.02.2023/Sultanlı Fuad Collections Homework 08.02.2023/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sultanlı_Fuad_Collections_Homework_08._02._2023
+{
+    public class Group
+    {
+        public string Name { get; set; }
+
+        public string GetDetails()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Sultanlı Fuad Collections Homework 08.02.2023/Sultanlı Fuad Collections Homework 08.02.2023/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Sultanlı_Fuad_Collections_Homework_08._02._2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Group> team = new List<Group>();
+
+            var group = new Group
+            {
+                Name = "P331"
+            };
+
+            Checker(team, group);
+
+            var group2 = new Group
+            {
+                Name = "P228"
+            };
+            Checker(team, group2);
+
+            foreach (var item in team)
+            {
+                Console.WriteLine(item.GetDetails());
+            }
+        }
+
+        public static void Checker(List<Group> team, Group group)
+        {
+            var isDublicatedId = team.Any(p => p.Name == group.Name);
+            if (!isDublicatedId)
+            {
+                team.Add(group);
+            }
+        }
+    }
+}
